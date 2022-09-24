c4250f09833c04beac2f6b0ea898a1267ba9e325
natenettey <niibobby4@gmail.com>
2022-09-24 10:24:02 +0000

    add the router for the passport.js configs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 require('dotenv').config()
+const passportRouter = require('./routes/passport_configs')
 
 
 //db connection
@@ -17,10 +18,15 @@ app.set('view engine','ejs')
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/forms',passportRouter)
 
-app.get('/',(req,res)=>{
-    res.render('signup')
+
+// app.post('/signup',(req, res, next)=>{
+//     console.log(req.body)
+// })
+
+app.get('/login',(req,res)=>{
+    res.render('login')
 })
-
 
 app.listen(5000)
