a446ea8866e77c56119ba137b3b9fb509ed24642
natenettey <niibobby4@gmail.com>
2022-10-06 21:01:44 +0000

    passed retievd document to the dashboard view

--- controllers/dashboard_controller.js
+++ controllers/dashboard_controller.js
@@ -1,7 +1,27 @@
-exports.getdashboard = (req, res)=>{
-   res.render('home',{user:req.user})
+const bcrypt = require('bcryptjs/dist/bcrypt')
+const files = require ('../models/file')
+
+exports.getdashboard = async (req, res)=>{
+   const userFiles = await files.find({user:req.user.email}).sort({originalName:1})
+   console.log(userFiles)
+   res.render('home',{user:req.user,docs:userFiles})
 }
 
-exports.uploadFile = (req,res)=>{
-   console.log(req.file)
-}
+// exports.uploadFile = async (req,res)=>{
+//    console.log(req.file,req.user.user_name)
+//    const name = req.user.email
+//    const newFile  = await new fileUpload({
+//       user:name,
+//       filePath:req.file.path,
+//       originalName:req.file.originalname
+//    }).save(error=>{
+//       if(error){
+//          console.log(error)
+//       }else{
+//       console.log("file uploaded")
+//       res.redirect('/account/dashboard/home')
+//       }
+//    }
+//    )
+   
+// }
