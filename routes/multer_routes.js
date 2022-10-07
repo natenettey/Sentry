55f26486bcfefbb2230c1f858f53cf33871d50e6
natenettey <niibobby4@gmail.com>
2022-10-07 22:07:21 +0000

    added the route for file download

--- routes/multer_routes.js
+++ routes/multer_routes.js
@@ -5,7 +5,8 @@ const upload = multer({dest:"uploads"})
 
 const multer_controller = require('../controllers/multer_controller')
 router.post('/upload',upload.single('file'),multer_controller.uploadFile)
-
+router.get('/download/:id',multer_controller.passwordFrom)
+router.post('/download/:id',multer_controller.downloadFile)
 
 
 module.exports = router
